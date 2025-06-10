9e89026cbdeac5f1427cbd8c6fe4d7bd096624fe
paygoline <paygoofficial1@gmail.com>
2025-06-10 16:19:03 +0100

    Update Support.tsx

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -4,11 +4,11 @@ import { ArrowLeft, MessageCircle } from 'lucide-react';
 
 const Support = ({ onBack }: { onBack: () => void }) => {
   const handleWhatsAppChat = () => {
-    window.open('https://wa.me/2348037750681', '_blank');
+    window.open('https://wa.me/2347086445037', '_blank');
   };
 
   const handleLiveChat = () => {
-    window.open('https://t.me/bluepay247', '_blank');
+    window.open('t.me/Paygofficial', '_blank');
   };
 
   return (
